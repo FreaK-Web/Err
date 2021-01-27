799d8e9d342a1585be90f5ef29695a90a2a94ed6
Freak_Web <2x2willbe4@gmail.com>
2021-01-27 22:48:55 +0300

    add Js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-const ans = prompt('Азактам еблан???', "")
+const NumOfFilms = +prompt('Сколько фильмов вы посмотрели?', "");
+
+const PerMovie = {
+    count : NumOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+const a = prompt('Один из посл просмотренных фильмов ?',"" ),
+      b = prompt('На сколько оцените его?', ""), 
+      c = prompt('Один из посл просмотренных фильмов ?',"" ),
+      d = prompt('На сколько оцените его?', "");
+
+ PerMovie.movies[a]= b;
+ PerMovie.movies[c]= d;
+
+
+console.log(PerMovie)
